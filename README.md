b041fee209daeece1f486655021cc31248e013a3
Stepko <sguralmen@gmail.com>
2023-04-09 18:18:51 +0300

    0.1.5
    Bit optimisated
    With new functions
    More universal

--- README.md
+++ README.md
@@ -15,13 +15,29 @@ bool isClicked();            //returns true if button is clicked
 bool isReleased();           //returns true if button is released
 bool isHeld();               //returns true if button is pressed in timeout
 bool isHold();               //returns true if button is pressed after timeout
+
 bool hasSingle();            //returns true if button is clicked once
 bool hasDouble();            //returns true if button is clicked twice
 bool hasTriple();            //returns true if button is clicked three times
 
-uint8_t hasClicks();         //returns clicks 
+uint8_t hasClicks();         //returns clicks after stopping clicking
+uint8_t hasClicksNow();      //returns clicks
 uint8_t hasClicksWithHeld(); //returns clicks before pressing
 uint8_t hasClicksWithHold(); //returns clicks before holding
+
+uint16_t holdInMillis();     //returns holding time after released
+uint16_t holdingTime();      //returns pressing time
+```
+## Redefinition
+```cpp
+// use this if you don't use Arduino framework or if you use alternative of these functions 
+#define EMFB_SETUP_FUNC(pin, pinmode) pinMode(pin, (pinmode) ? INPUT : INPUT_PULLUP);
+#define EMFB_READ_FUNC(pin, pinmode) digitalRead(pin) == (pinmode) ? HIGH : LOW
+#define EMFB_MILLIS_FUNC millis()
+// timers
+#define EMFB_DEB_TIMER 50
+#define EMFB_HOLD_TIMER 500
+#define EMFB_RELEASE_TIMER 500
 ```
 ## Using example
 ```cpp
--- src/EMFButton.h
+++ src/EMFButton.h
@@ -2,15 +2,32 @@
 #define EMFB_BUTTON_H
 #include "Arduino.h"
 
-#define EMFB_DEB_TIMER 50
-#define EMFB_HOLD_TIMER 500
-#define EMFB_RELEASE_TIMER 500
+//=============================
+#ifndef EMFB_SETUP_FUNC
+#define EMFB_SETUP_FUNC(pin, pinmode) pinMode(pin, (pinmode) ? INPUT : INPUT_PULLUP)
+#endif
+#ifndef EMFB_READ_FUNC
+#define EMFB_READ_FUNC(pin, pinmode) digitalRead(pin) == (pinmode) ? HIGH : LOW
+#endif
+#ifndef EMFB_MILLIS_FUNC
+#define EMFB_MILLIS_FUNC millis()
+#endif
 
+#ifndef EMFB_DEB_TIMER
+#define EMFB_DEB_TIMER 50
+#endif
+#ifndef EMFB_HOLD_TIMER
+#define EMFB_HOLD_TIMER 500
+#endif
+#ifndef EMFB_RELEASE_TIMER
+#define EMFB_RELEASE_TIMER 500
+#endif
+//=============================
 #define HIGH_P 0
 #define LOW_P 1
 #define NORM_OPEN 0
 #define NORM_CLOSED 1
-
+//=============================
 #pragma pack(push,1)
 typedef struct {
   bool pinmode: 1;
@@ -23,12 +40,9 @@ typedef struct {
   bool hold: 1;
 } EMFB_flags;
 #pragma pack(pop)
-
+//============================
 class EMFButton {
   public:
-    EMFButton(uint8_t Npin, bool Npinmode = 0, bool Npinclosed = 0);
-
-    void tick();
 
     bool isPressed() {
       return flag.pressed;
@@ -49,11 +63,36 @@ class EMFButton {
     uint8_t hasClicks() {
       return _clicksEnd;
     }
+    uint8_t hasClicksNow() {
+      if (flag.pressed)
+        return _clicks - 1;
+      else
+        return _clicks;
+    }
     uint8_t hasClicksWithHeld() {
-      return _clicksWithHeld;
+      if (flag.held)
+        return _clicks - 1;
+      else
+        return 0;
     }
     uint8_t hasClicksWithHold() {
-      return _clicksWithHold;
+      if (flag.hold)
+        return _clicks - 1;
+      else
+        return 0;
+    }
+
+    uint16_t holdingTime() {
+      if (flag.pressed)
+        return EMFB_MILLIS_FUNC - _timer;
+      else
+        return 0;
+    }
+    uint16_t holdinMillis() {
+      if (flag.released && !mode)
+        return _timer;
+      else
+        return 0;
     }
 
     bool hasSingle() {
@@ -65,19 +104,88 @@ class EMFButton {
     bool hasTriple() {
       return _clicksEnd == 3;
     }
+    
+    EMFButton(uint8_t Npin, bool Npinmode = 0, bool Npinclosed = 0) {
+      _pin = Npin;
+      flag.pinmode = Npinmode;
+      flag.pinclosed = Npinclosed;
+      pinMode(_pin, (flag.pinmode) ? INPUT : INPUT_PULLUP);
+    }
 
+    void tick() {
+      flag.clicked = 0;
+      flag.held = 0;
+      flag.hold = 0;
+      flag.released = 0;
+      _clicksEnd = 0;
+      uint16_t t = EMFB_MILLIS_FUNC;
+      bool reading = (!flag.pinclosed)^ EMFB_READ_FUNC(_pin, flag.pinmode);
+
+      if (reading != flag.lastState) {
+        _lastChange = t;
+      }
+      if (t - _lastChange > EMFB_DEB_TIMER) {
+        flag.pressed = !reading;
+
+        if (flag.pressed) {
+          switch (mode) {
+
+            case await:
+              mode = pressed;
+              _timer = t;
+              _clicks = 1;
+              flag.clicked = 1;
+              break;
+
+            case pressed:
+              if (t - _timer >= EMFB_HOLD_TIMER) {
+                mode = held;
+                flag.held = 1;
+              }
+              break;
+
+            case held:
+              flag.hold = 1;
+              break;
+
+            case released:
+              mode = pressed;
+              _timer = t;
+              _clicks ++;
+              flag.clicked = 1;
+              break;
+          }
+        } else {
+          switch (mode) {
+            case pressed:  if (_clicks == 15) {
+                mode = await;
+                _clicksEnd = _clicks;
+                _clicks = 0;
+              } else mode = released; _timer = t; flag.released = 1; break;
+
+            case held: mode = await; _timer = t - _timer; _clicks = 0; flag.released = 1; break;
+
+            case released: if (t - _timer >= EMFB_RELEASE_TIMER) {
+                mode = await;
+                _timer = t;
+                _clicksEnd = _clicks;
+                _clicks = 0;
+              } break;
+          }
+        }
+      }
+      flag.lastState = reading;
+    };
   private:
     EMFB_flags flag;
     uint8_t _pin;
 
     uint8_t _clicks: 4;
     uint8_t _clicksEnd: 4;
-    uint8_t _clicksWithHeld: 4;
-    uint8_t _clicksWithHold: 4;
 
     enum button_state {await, pressed, held, released} mode: 2;
 
-    uint16_t _timer;
-    uint16_t _lastChange;
- };
+      uint16_t _timer;
+      uint16_t _lastChange;
+  };
 #endif
